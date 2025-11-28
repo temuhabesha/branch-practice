68488e67d3629900e8f72b5dca3db3856fb163b7
bezaadefris1212 <bezaadefris06@gmail.com>
2025-11-28 14:50:00 -0800

    this is the commit when change

--- app/login.jsx
+++ app/login.jsx
@@ -1,14 +1,18 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 const login = () => {
   return (
     <View>
-      <Text>this is login screen</Text>
-      <Text>this is branch of bezu</Text>
-      <Text>so if you want my code i can send pull request</Text>
+      <Text>hi temu how was a nignt ?</Text>
     </View>
   );
 }
 
-export default login
+export default login;
+
+const style = StyleSheet.create({
+    logintext:{
+        color:"white"
+    }
+})
